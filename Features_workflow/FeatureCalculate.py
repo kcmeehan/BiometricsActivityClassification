868a344c252bd1cb0b9eabc33158fe467d6e23b1
Robert <rmartin-short@berkeley.edu>
2018-07-29 11:44:06 -0700

    fixed redaction bug. Added classification test

--- Features_workflow/FeatureCalculate.py
+++ Features_workflow/FeatureCalculate.py
@@ -22,20 +22,22 @@ class FeatureCalc:
 
 	def load_new_ts(self,input_df):
 
-		'''Load a new data chunk for analysis'''
+		'''
+        Load a new data chunk for analysis 
+        '''
 
 		self.indf = input_df
 
 		#Feature columns for three component analysis
 		self.hand_acc = self.indf[['hand_acc16g_x','hand_acc16g_y','hand_acc16g_z']]
 		self.hand_gyro = self.indf[['hand_gyro_x','hand_gyro_y','hand_gyro_z']]
-		self.hand_mag = self.indf[['hand_mag_x','hand_mag_y','hand_mag_z']]
+		#self.hand_mag = self.indf[['hand_mag_x','hand_mag_y','hand_mag_z']]
 		self.chest_acc = self.indf[['chest_acc16g_x','chest_acc16g_y','chest_acc16g_z']]
 		self.chest_gyro = self.indf[['chest_gyro_x','chest_gyro_y','chest_gyro_z']]
-		self.chest_mag = self.indf[['chest_mag_x','chest_mag_y','chest_mag_z']]
+		#self.chest_mag = self.indf[['chest_mag_x','chest_mag_y','chest_mag_z']]
 		self.ankle_acc = self.indf[['ankle_acc16g_x','ankle_acc16g_y','ankle_acc16g_z']]
 		self.ankle_gyro = self.indf[['ankle_gyro_x','ankle_gyro_y','ankle_gyro_z']]
-		self.ankle_mag = self.indf[['ankle_mag_x','ankle_mag_y','ankle_mag_z']]
+		#self.ankle_mag = self.indf[['ankle_mag_x','ankle_mag_y','ankle_mag_z']]
 
 	def calculate_features(self):
 
@@ -66,17 +68,41 @@ class FeatureCalc:
 	#########################
 
 	def mean(self):
+        
+        '''
+        Return mean of all the columns in the df
+        '''
 
-		return self.indf.mean().values
+		return self.indf.mean(skipna=False).values
 
 	def median(self):
+        
+        '''
+        Return median of all the columns in the df
+        '''
 
-		return self.indf.median().values
+		return self.indf.median(skipna=False).values
 
 	def std(self):
+        
+        '''
+        Return std of all the columns in the df
+        '''
 
-		return self.indf.std().values
+		return self.indf.std(skipna=False).values
 
 	def peak(self):
+        
+        '''
+        Return peak of all the columns in the df
+        '''
 
 		return self.indf.abs().max().values
+    
+    def kurtotis(self):
+        
+        '''
+        Return kurtosis of all the columns in the df
+        '''
+        
+        return self.indf.kurt(skipna=False).values
